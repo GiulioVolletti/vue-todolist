99406dc7a8ad8e0d9d77b8da70955565d66a7a08
Giulio <giuliovolletti@hotmail.com>
2020-12-10 12:04:04 +0100

    commit1

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Esercizio:
+// - aggiungere un campo di input e un pulsante per aggiungere nuove cose da fare alla lista;
+// - cancellare un item dalla lista, al click sulla relativa icona 'X'.
+// Bonus:
+// - Oltre al click sul pulsante, consentire l'inserimento del nuovo item anche premendo il tasto Invio della tastiera, dopo aver scritto il testo nel campo di input.
+
+
+console.log("hello world");
+var list = new Vue (
+  {
+    el: '#list_item',
+    data: {
+      // - creare una lista di cose da fare e mostrarla in pagina con Vue;
+      toDoList:[
+        "cucinare",
+        "fare benzia",
+        "fare la spesa",
+        "comprare il giornale"
+      ],
+      
+      message: "hello world",
+    },
+    methods:{
+      deleteItem: function(){
+        console.log("here");
+
+      },
+    },
+  }
+);
